375d7811a69a5c3270d16b33544abaf02e7a4ae5
Francois Blackburn <fblackburn@wazo.community>
2017-12-19 11:23:00 -0500

    add features commands

---
+++ xivo_confd_client/commands/features_featuremap.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+from xivo_lib_rest_client import RESTCommand
+
+
+class FeaturesFeaturemapCommand(RESTCommand):
+
+    resource = 'asterisk/features/featuremap'
+
+    def get(self):
+        response = self.session.get(self.resource)
+        return response.json()
+
+    def update(self, body):
+        self.session.put(self.resource, body)
