4071fa48e384ba2609c879dbee2f7e7e6303005b
Pascal Cadotte Michaud <pcm@wazo.io>
2018-03-29 14:14:26 -0400

    entities: handle the tenant_uuid argument

--- xivo_confd_client/commands/entities.py
+++ xivo_confd_client/commands/entities.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2016 Avencall
+# Copyright 2016-2018 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
+from xivo_confd_client.util import url_join
 from xivo_confd_client.crud import CRUDCommand
 
 
 class EntitiesCommand(CRUDCommand):
 
     resource = 'entities'
+
+    def create(self, body):
+        headers = self.session.WRITE_HEADERS
+        tenant_uuid = body.pop('tenant_uuid', self._client.tenant())
+        if tenant_uuid:
+            headers['Wazo-Tenant'] = tenant_uuid
+
+        url = url_join(self.resource)
+        response = self.session.post(url, body, headers=headers)
+        return response.json()
