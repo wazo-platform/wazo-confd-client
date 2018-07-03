c086cee99d8010e9e7d1e445889ca0119aa7c4f1
Pascal Cadotte Michaud <pcm@wazo.io>
2018-07-03 14:48:01 -0400

    incalls: make the command multi-tenant

--- xivo_confd_client/commands/incalls.py
+++ xivo_confd_client/commands/incalls.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016-2017 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2016-2018 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
 from xivo_confd_client.util import extract_id
-from xivo_confd_client.crud import CRUDCommand
+from xivo_confd_client.crud import MultiTenantCommand
 from xivo_confd_client.relations import IncallExtensionRelation, IncallScheduleRelation
 
 
@@ -31,7 +31,7 @@ class IncallRelation(object):
         return self.incall_schedule.dissociate(self.incall_id, schedule_id)
 
 
-class IncallsCommand(CRUDCommand):
+class IncallsCommand(MultiTenantCommand):
 
     resource = 'incalls'
     relation_cmd = IncallRelation
