78514a8124573f73385388d83122e094b26257fd
Sebastien Duthil <sduthil@wazo.community>
2019-02-12 13:42:59 -0500

    sounds: add multi-tenancy for all actions

--- xivo_confd_client/commands/sounds.py
+++ xivo_confd_client/commands/sounds.py
@@ -12,20 +12,50 @@ class SoundsCommand(MultiTenantCommand):
 
     resource = 'sounds'
 
+    def get(self, category, tenant_uuid=None):
+        tenant_uuid = tenant_uuid or self._client.tenant()
+        headers = dict(self.session.READ_HEADERS)
+        if tenant_uuid:
+            headers['Wazo-Tenant'] = tenant_uuid
+
+        url = url_join(self.resource, category)
+        response = self.session.get(url, headers=headers)
+        return response.json()
+
+    def delete(self, category, tenant_uuid=None):
+        tenant_uuid = tenant_uuid or self._client.tenant()
+        headers = dict(self.session.READ_HEADERS)
+        if tenant_uuid:
+            headers['Wazo-Tenant'] = tenant_uuid
+
+        url = url_join(self.resource, category)
+        response = self.session.delete(url, headers=headers)
+        return response.json()
+
     @extract_id
     def download_file(self, category, filename, **kwargs):
-        url = url_join(self.resource, category, 'files', quote(filename, safe=''))
+        tenant_uuid = kwargs.pop('tenant_uuid', None) or self._client.tenant()
         headers = {'Accept': '*/*'}
+        if tenant_uuid:
+            headers['Wazo-Tenant'] = tenant_uuid
+        url = url_join(self.resource, category, 'files', quote(filename, safe=''))
         response = self.session.get(url, headers=headers, params=kwargs)
         return response
 
     @extract_id
     def upload_file(self, category, filename, content, **kwargs):
-        url = url_join(self.resource, category, 'files', filename)
+        tenant_uuid = kwargs.pop('tenant_uuid', None) or self._client.tenant()
         headers = {'Content-Type': 'application/octet-stream'}
+        if tenant_uuid:
+            headers['Wazo-Tenant'] = tenant_uuid
+        url = url_join(self.resource, category, 'files', filename)
         self.session.put(url, raw=content, headers=headers, params=kwargs)
 
     @extract_id
     def delete_file(self, category, filename, **kwargs):
+        tenant_uuid = kwargs.pop('tenant_uuid', None) or self._client.tenant()
+        headers = {}
+        if tenant_uuid:
+            headers['Wazo-Tenant'] = tenant_uuid
         url = url_join(self.resource, category, 'files', filename)
-        self.session.delete(url, params=kwargs)
+        self.session.delete(url, headers=headers, params=kwargs)
