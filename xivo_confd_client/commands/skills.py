46da8562a37d273b4ee5cff035688b9b9755fad4
Alexandre Fournier <afournier@wazo.io>
2019-05-06 13:12:50 -0400

    skills: use MultiTenantCommand

--- xivo_confd_client/commands/skills.py
+++ xivo_confd_client/commands/skills.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from xivo_confd_client.crud import CRUDCommand
+from xivo_confd_client.crud import MultiTenantCommand
 
 
-class SkillsCommand(CRUDCommand):
+class SkillsCommand(MultiTenantCommand):
 
     resource = 'agents/skills'
