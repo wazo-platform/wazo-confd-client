2016f37c25226e72e5f707f53e83591a191191fc
Pascal Cadotte Michaud <pcm@wazo.io>
2020-02-14 14:30:20 -0500

    add the pjsip_global command

---
+++ wazo_confd_client/commands/pjsip_global.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from wazo_lib_rest_client import RESTCommand
+
+
+class PJSIPGlobalCommand(RESTCommand):
+
+    resource = 'asterisk/pjsip/global'
+
+    def get(self):
+        response = self.session.get(self.resource)
+        return response.json()
+
+    def update(self, body):
+        self.session.put(self.resource, body)
