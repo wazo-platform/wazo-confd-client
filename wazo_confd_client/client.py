b6c810a068a90daf27f3dd71376930f463c53dcb
Francois Blackburn <fblackburn@wazo.community>
2020-04-30 12:33:15 -0400

    remove unused basic auth

--- wazo_confd_client/client.py
+++ wazo_confd_client/client.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
-# Copyright 2015-2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2015-2020 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
-
-import requests
 
 from wazo_lib_rest_client.client import BaseClient
 from wazo_confd_client.session import ConfdSession
@@ -13,16 +11,12 @@ class ConfdClient(BaseClient):
     namespace = 'wazo_confd_client.commands'
 
     def __init__(
-        self, host, port=9486, version='1.1', username=None, password=None, **kwargs
+        self, host, port=9486, version='1.1', **kwargs
     ):
         super(ConfdClient, self).__init__(
             host=host, port=port, version=version, **kwargs
         )
-        self.username = username
-        self.password = password
 
     def session(self):
         session = super(ConfdClient, self).session()
-        if self.username and self.password:
-            session.auth = requests.auth.HTTPDigestAuth(self.username, self.password)
         return ConfdSession(session, self.url())
