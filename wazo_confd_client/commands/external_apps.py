568980d09f55c73e9b796588d5f23c76e647e811
Francois Blackburn <fblackburn@wazo.community>
2020-10-29 12:07:10 -0400

    refactor external_app to use common helper

--- wazo_confd_client/commands/external_apps.py
+++ wazo_confd_client/commands/external_apps.py
@@ -4,50 +4,33 @@
 
 import six
 
-from functools import wraps
-
-from wazo_confd_client.util import url_join
+from wazo_confd_client.util import url_join, extract_name
 from wazo_confd_client.crud import MultiTenantCommand
-
-
-def extract_name(func):
-    @wraps(func)
-    def wrapper(self, resource_or_id, *args, **kwargs):
-        if isinstance(resource_or_id, dict):
-            if 'name' in resource_or_id:
-                resource_id = resource_or_id['name']
-            else:
-                raise KeyError('no "name" key found')
-        else:
-            resource_id = resource_or_id
-        return func(self, resource_id, *args, **kwargs)
-
-    return wrapper
 
 
 class ExternalAppsCommand(MultiTenantCommand):
 
     resource = 'external/apps'
 
-    @extract_name
+    @extract_name()
     def get(self, resource_id):
         url = url_join(self.resource, resource_id)
         response = self.session.get(url)
         return response.json()
 
-    def create(self, body):
-        resource_id = body.pop('name', None)
-        url = url_join(self.resource, resource_id)
+    @extract_name(pass_original=True)
+    def create(self, name, body):
+        url = url_join(self.resource, name)
         response = self.session.post(url, body)
         return response.json()
 
-    def update(self, body):
-        resource_id = body.get('name')
-        url = url_join(self.resource, resource_id)
+    @extract_name(pass_original=True)
+    def update(self, name, body):
+        url = url_join(self.resource, name)
         body = {key: value for key, value in six.iteritems(body) if key != "links"}
         self.session.put(url, body)
 
-    @extract_name
+    @extract_name()
     def delete(self, resource_id):
         url = url_join(self.resource, resource_id)
         self.session.delete(url)
