595cae13fc28649ee6c80284ae7c69d2d1830f0c
Alexandre Fournier <afournier@wazo.io>
2021-03-18 11:55:46 -0400

    emails: use RESTCommand instead of HTTPCommand

--- wazo_confd_client/commands/emails.py
+++ wazo_confd_client/commands/emails.py
@@ -2,20 +2,16 @@
 # Copyright 2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from wazo_lib_rest_client import HTTPCommand
-from wazo_confd_client.util import url_join
+from wazo_lib_rest_client import RESTCommand
 
 
-class EmailsCommand(HTTPCommand):
+class EmailsCommand(RESTCommand):
 
-    headers = {'Accept': 'application/json'}
+    resource = 'emails'
 
     def get(self):
-        url = url_join('emails')
-        r = self.session.get(url, headers=self.headers)
-
+        r = self.session.get(self.resource)
         return r.json()
 
     def update(self, body):
-        url = url_join('emails')
-        self.session.put(url, json=body, headers=self.headers)
+        self.session.put(self.resource, body)
