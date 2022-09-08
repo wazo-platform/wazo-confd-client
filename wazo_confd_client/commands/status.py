fe2ac7870362663d5bafc3c4386f51b097bdb93e
nissou91 <achohra@wazo.io>
2022-09-08 13:39:38 -0400

    Add exception handling

--- wazo_confd_client/commands/status.py
+++ wazo_confd_client/commands/status.py
@@ -2,10 +2,10 @@
 # Copyright 2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from wazo_lib_rest_client.command import RESTCommand
+from wazo_confd_client.command import ConfdCommand
 
 
-class StatusCommand(RESTCommand):
+class StatusCommand(ConfdCommand):
     resource = 'status'
 
     def __call__(self):
