f94b27ac57bebac205d0ba49c50bf6a03afd0d41
nissou91 <achohra@wazo.io>
2022-09-14 11:15:37 -0400

    remove unused files and use HTTPCommand

--- wazo_confd_client/commands/status.py
+++ wazo_confd_client/commands/status.py
@@ -2,18 +2,14 @@
 # Copyright 2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from wazo_confd_client.command import ConfdCommand
+from wazo_lib_rest_client import HTTPCommand
 
 
-class StatusCommand(ConfdCommand):
-    resource = 'status'
-
+class StatusCommand(HTTPCommand):
     def __call__(self):
-        headers = self._get_headers()
-        url = self.base_url
-        r = self.session.get(url, headers=headers)
+        return self.get()
 
-        if r.status_code != 200:
-            self.raise_from_response(r)
+    def get(self):
+        r = self.session.get('/status')
 
         return r.json()
--- wazo_confd_client/exceptions.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2022 The Wazo Authors  (see the AUTHORS file)
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-
-class ConfdClientError(Exception):
-    def __init__(self, error):
-        super(ConfdClientError, self).__init__(error)
-        self.error = error
