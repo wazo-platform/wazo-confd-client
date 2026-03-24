6c6ef4cfd3f8911117290b62a0b9fd522b69b841
Charles Langlois <charles.langlois@wazo.io>
2026-03-24 16:58:38 -0400

    voicemail transcription: uniformize tests

--- wazo_confd_client/commands/tests/test_voicemail_transcription.py
+++ wazo_confd_client/commands/tests/test_voicemail_transcription.py
@@ -1,13 +1,15 @@
 # Copyright 2026 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from unittest.mock import ANY
-
 from hamcrest import assert_that, equal_to
 
+from wazo_confd_client.session import ConfdSession
 from wazo_confd_client.tests import TestCommand
 
 from ..voicemail_transcription import VoicemailTranscriptionCommand
+
+READ_HEADERS = dict(ConfdSession.READ_HEADERS)
+WRITE_HEADERS = dict(ConfdSession.WRITE_HEADERS)
 
 
 class TestVoicemailTranscription(TestCommand):
@@ -22,18 +24,35 @@ class TestVoicemailTranscription(TestCommand):
         assert_that(result, equal_to(expected))
         self.session.get.assert_called_once_with(
             '/voicemails/transcription',
-            headers=ANY,
+            headers=READ_HEADERS,
             params={},
         )
 
     def test_get_with_tenant_uuid(self):
-        tenant_uuid = 'my-tenant-uuid'
+        self.client.tenant_uuid = None
+        expected_headers = {**READ_HEADERS, 'Wazo-Tenant': 'my-tenant-uuid'}
         self.set_response('get', 200, {'enabled': False})
 
-        self.command.get(tenant_uuid=tenant_uuid)
+        self.command.get(tenant_uuid='my-tenant-uuid')
 
-        call_kwargs = self.session.get.call_args
-        assert_that(call_kwargs.kwargs['headers']['Wazo-Tenant'], equal_to(tenant_uuid))
+        self.session.get.assert_called_once_with(
+            '/voicemails/transcription',
+            headers=expected_headers,
+            params={},
+        )
+
+    def test_get_uses_client_tenant_uuid(self):
+        self.client.tenant_uuid = 'client-tenant'
+        expected_headers = {**READ_HEADERS, 'Wazo-Tenant': 'client-tenant'}
+        self.set_response('get', 200, {'enabled': True})
+
+        self.command.get()
+
+        self.session.get.assert_called_once_with(
+            '/voicemails/transcription',
+            headers=expected_headers,
+            params={},
+        )
 
     def test_update(self):
         self.client.tenant_uuid = None
@@ -45,39 +64,36 @@ class TestVoicemailTranscription(TestCommand):
         self.session.put.assert_called_once_with(
             '/voicemails/transcription',
             json=body,
-            headers=ANY,
+            headers=WRITE_HEADERS,
             params={},
         )
 
     def test_update_with_tenant_uuid(self):
-        tenant_uuid = 'my-tenant-uuid'
+        self.client.tenant_uuid = None
+        expected_headers = {**WRITE_HEADERS, 'Wazo-Tenant': 'my-tenant-uuid'}
         self.set_response('put', 204)
         body = {'enabled': True}
 
-        self.command.update(body, tenant_uuid=tenant_uuid)
+        self.command.update(body, tenant_uuid='my-tenant-uuid')
 
-        call_kwargs = self.session.put.call_args
-        assert_that(call_kwargs.kwargs['headers']['Wazo-Tenant'], equal_to(tenant_uuid))
-
-    def test_get_uses_client_tenant_uuid(self):
-        self.client.tenant_uuid = 'client-tenant'
-        self.set_response('get', 200, {'enabled': True})
-
-        self.command.get()
-
-        call_kwargs = self.session.get.call_args
-        assert_that(
-            call_kwargs.kwargs['headers']['Wazo-Tenant'], equal_to('client-tenant')
+        self.session.put.assert_called_once_with(
+            '/voicemails/transcription',
+            json=body,
+            headers=expected_headers,
+            params={},
         )
 
     def test_update_uses_client_tenant_uuid(self):
         self.client.tenant_uuid = 'client-tenant'
+        expected_headers = {**WRITE_HEADERS, 'Wazo-Tenant': 'client-tenant'}
         self.set_response('put', 204)
         body = {'enabled': True}
 
         self.command.update(body)
 
-        call_kwargs = self.session.put.call_args
-        assert_that(
-            call_kwargs.kwargs['headers']['Wazo-Tenant'], equal_to('client-tenant')
+        self.session.put.assert_called_once_with(
+            '/voicemails/transcription',
+            json=body,
+            headers=expected_headers,
+            params={},
         )
